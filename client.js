ec330c0901c5bdd820bb1e215280785e7e66e15a
Reynanwq <reynanwq@gmail.com>
2023-11-29 16:38:50 -0300

    trying to connect with the client

---
+++ client.js
@@ -0,0 +1,21 @@
+const { io } = require("socket.io-client");
+
+const socket = io("http://localhost:3000");
+
+socket.on("connect", () => {
+    console.log(`Connected to server with ID: ${socket.id}`);
+
+    // Teste os comandos aqui
+    socket.emit('command', 'list client1');
+    socket.emit('command', 'deposit client1 2 test.txt Hello, World!');
+    socket.emit('command', 'recover client1 test.txt');
+    socket.emit('command', 'delete client1 test.txt');
+});
+
+socket.on("message", (message) => {
+    console.log(`[CLIENT] ${message}`);
+});
+
+socket.on("disconnect", () => {
+    console.log("[CLIENT] Disconnected from server");
+});
